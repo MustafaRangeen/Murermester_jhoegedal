c235e1a26d20653289bbb7d505992f4ab6b8b2c8
Mustafa Rangeen <33625111+MustafaRangeen@users.noreply.github.com>
2020-03-07 03:57:16 +0100

    Add files via upload

---
+++ mail_handler.php
@@ -0,0 +1,23 @@
+<?php
+    if(isset($_POST['Sendmsg'])){
+        $Navn=$_POST['Navn'];
+        $Efternavn=$_POST['Efternavn'];
+        $Tlf=$_POST['Tlf'];
+        $mail=$_POST['mail'];
+        $besked=$_POST['besked'];
+
+
+
+        $to='mmrangeen@gmail.com';
+        $subject='Form Murermester Jørgen Høgedal ApS';
+        $message="Navn: ".$Navn."\n"."Efternavn: ".$Efternavn."\n"."Tlf: ".$Tlf."\n"."mail: ".$mail."\n"."Besked: ".$besked."\n"."\n";
+        $headers="From: ".$mail;
+
+        if(mail($to, $subject, $message, $headers)){
+            echo "<h1> Tak for din henvendelse"." ".$Navn.", du vil blive kontaktet snarest muligt</h1>";
+        }
+        else{
+            echo "Noget gik galt, prøv igen";
+        }
+    }
+?>
